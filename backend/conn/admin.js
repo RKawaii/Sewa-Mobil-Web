fd451dab1f8c88ebbeb6715c138582edaa67eb79
RKawaii <tazakeren8@gmail.com>
2019-08-17 14:37:09 +0700

    template backend dan truktur kode

---
+++ backend/conn/admin.js
@@ -0,0 +1,30 @@
+const mysql = require('mysql2');
+
+//var uuid = require('uuid/v4');
+
+//local mysql db connection
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'roots',
+  password: '',
+  database: 'arphat'
+});
+
+connection.connect(function(err) {
+  if (err) console.log(err, 'cant establish connection to mysql');
+  else console.log('admin connection to mysql established');
+});
+
+module.exports = {
+  get: function(req, res) {
+    //console.log(req);
+
+    connection.query(`select * from mobil`, function(err, results, fields) {
+      //console.log(err);
+      console.log(results);
+      //console.log(fields);
+
+      res.sendStatus(200);
+    });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,129 @@
+//express core
 const express = require('express');
 const app = express();
 const PORT = 5000;
+const Route = express.Router();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+//additional plugin
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const Joi = require('@hapi/joi');
+const validator = require('express-joi-validation').createValidator({});
+
+//mysql connection
+const admin = require('./conn/admin');
+
+//Express Route
+//untuk mobil
+Route.get('/mobil', function(req, res) {
+  admin.get(req, res);
+});
+Route.post('/mobil', function(req, res) {
+  res.json({ hello: 'world' });
+});
+Route.put('/mobil', function(req, res) {
+  res.json({ hello: 'world' });
+});
+Route.delete('/mobil', function(req, res) {
+  res.json({ hello: 'world' });
+});
+//untuk sewa
+Route.get('/sewa', function(req, res) {
+  res.json({ hello: 'world' });
+});
+Route.post('/sewa', function(req, res) {
+  res.json({ hello: 'world' });
+});
+Route.put('/sewa', function(req, res) {
+  res.json({ hello: 'world' });
+});
+Route.delete('/sewa', function(req, res) {
+  res.json({ hello: 'world' });
+});
+
+//untuk pelaggan
+Route.get('/pelanggan', function(req, res) {
+  res.json({ hello: 'world' });
+});
+Route.post('/pelanggan', function(req, res) {
+  res.json({ hello: 'world' });
+});
+Route.put('/pelanggan', function(req, res) {
+  res.json({ hello: 'world' });
+});
+Route.delete('/pelanggan', function(req, res) {
+  res.json({ hello: 'world' });
+});
+
+//untuk transaksi
+Route.get('/transaksi', function(req, res) {
+  res.json({ hello: 'world' });
+});
+Route.post('/transaksi', function(req, res) {
+  res.json({ hello: 'world' });
+});
+Route.put('/transaksi', function(req, res) {
+  res.json({ hello: 'world' });
+});
+Route.delete('/transaksi', function(req, res) {
+  res.json({ hello: 'world' });
+});
+
+//untuk staff
+Route.get('/staff', function(req, res) {
+  res.json({ hello: 'world' });
+});
+Route.post('/staff', function(req, res) {
+  res.json({ hello: 'world' });
+});
+Route.put('/staff', function(req, res) {
+  res.json({ hello: 'world' });
+});
+Route.delete('/staff', function(req, res) {
+  res.json({ hello: 'world' });
+});
+
+//untuk riwayat
+Route.get('/riwayat', function(req, res) {
+  res.json({ hello: 'world' });
+});
+Route.post('/riwayat', function(req, res) {
+  res.json({ hello: 'world' });
+});
+Route.put('/riwayat', function(req, res) {
+  res.json({ hello: 'world' });
+});
+Route.delete('/riwayat', function(req, res) {
+  res.json({ hello: 'world' });
+});
+
+/* buat validasi data nanti
+const test = Joi.object({
+  test: Joi.string().required()
+});
+Route.post('/mobil', validator.body(test), function(req, res) {
+  res.json({ hello: 'world' });
+});
+*/
+//init express
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(
+  session({
+    secret: 'Astolfo 1s n0t g4y',
+    resave: true,
+    saveUninitialized: false,
+    cookie: { maxAge: 0 }
+  })
+);
+
+app.use('/api', Route);
+app.use('/assets', express.static('assets'));
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}`));
